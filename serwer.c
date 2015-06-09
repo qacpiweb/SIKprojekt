08df832f4d3ca301f454052e6b82255465cd02c1
qacpiweb <qacpiweb@gmail.com>
2015-06-09 11:24:41 +0200

    final?

--- serwer.c
+++ serwer.c
@@ -134,67 +134,67 @@ int main(int argc, char *argv[]) {
    char kb_msg[MSG_SIZE + 10]; 
       
 
-   /*Server==================================================*/
+   /*Serwer==================================================*/
    if(argc==1 || argc == 3){
      if(argc==3){
        if(!strcmp("-p",argv[1])){
          sscanf(argv[2],"%i",&port);
        }else{
-         printf("Invalid parameter.\nUsage: chat [-p PORT] HOSTNAME\n");
+         printf("Nieprawidlowe wywolanie\n");
          exit(0);
        }
      }else port=MYPORT;
      
-     printf("\n*** Server program starting (enter \"quit\" to stop): \n");
+     printf("\n*** Uruchamianie serwera (wpisz quit aby zakonczyc): \n");
      fflush(stdout);
 
-     /* Create and name a socket for the server */
+     /* Socket dla serwera */
      server_sockfd = socket(AF_INET, SOCK_STREAM, 0);
      server_address.sin_family = AF_INET;
      server_address.sin_addr.s_addr = htonl(INADDR_ANY);
      server_address.sin_port = htons(port);
      bind(server_sockfd, (struct sockaddr *)&server_address, addresslen);
 
-     /* Create a connection queue and initialize a file descriptor set */
+     /* Kolejka polaczen, inicjalizacja zestawu deskryptorow */
      listen(server_sockfd, 1);
      FD_ZERO(&readfds);
      FD_SET(server_sockfd, &readfds);
-     FD_SET(0, &readfds);  /* Add keyboard to file descriptor set */
+     FD_SET(0, &readfds);  /* Klawiatura */
 
-     /*  Now wait for clients and requests */
+     /*  Oczekiwanie na klientow */
      while (1) {
         testfds = readfds;
         select(FD_SETSIZE, &testfds, NULL, NULL, NULL);
                     
-        /* If there is activity, find which descriptor it's on using FD_ISSET */
+        /* Jesli nastepuje aktywnosc sprawdzamy gdzie przy uzyciu FD_ISSET */
         for (fd = 0; fd < FD_SETSIZE; fd++) {
            if (FD_ISSET(fd, &testfds)) {
               
-              if (fd == server_sockfd) { /* Accept a new connection request */
+              if (fd == server_sockfd) { /* Zaakceptowanie nowego polaczenia */
                  client_sockfd = accept(server_sockfd, NULL, NULL);
                 
                                 
                  if (num_clients < MAX_CLIENTS) {
                     FD_SET(client_sockfd, &readfds);
                     fd_array[num_clients]=client_sockfd;
-                    /*Client ID*/
-                    printf("Client %d joined\n",num_clients++);
+                    /*ID klienta*/
+                    printf("Klient %d polaczony\n",num_clients++);
                     fflush(stdout);
                     
                     sprintf(msg,"M%2d",client_sockfd);
-                    /*write 2 byte clientID */
+                    /*wyslij ID klienta */
                     send(client_sockfd,msg,strlen(msg),0);
                  }
                  else {
-                    sprintf(msg, "XSorry, too many clients.  Try again later.\n");
+                    sprintf(msg, "Zbyt wiele klientow. Sprobuj pozniej.\n");
                     write(client_sockfd, msg, strlen(msg));
                     close(client_sockfd);
                  }
               }
-              else if (fd == 0)  {  /* Process keyboard activity */                 
+              else if (fd == 0)  {  /* obsluga klawiatury */                 
                  fgets(kb_msg, MSG_SIZE + 1, stdin);
                  if (strcmp(kb_msg, "quit\n")==0) {
-                    sprintf(msg, "XServer is shutting down.\n");
+                    sprintf(msg, "Serwer sie zamyka\n");
                     for (i = 0; i < num_clients ; i++) {
                        write(fd_array[i], msg, strlen(msg));
                        close(fd_array[i]);
@@ -208,13 +208,13 @@ int main(int argc, char *argv[]) {
                        write(fd_array[i], msg, strlen(msg));
                  }
               }
-              else if(fd) {  /*Process Client specific activity*/
-                 //read data from open socket
+              else if(fd) {  /*obsluga aktywnosci klientow*/
+                 //odczyt z otwartego sockeetu
                  result = read(fd, msg, MSG_SIZE);
                  
                  if(result==-1) perror("read()");
                  else if(result>0){
-                    /*read 2 bytes client id*/
+                    /*odczyt id klienta*/
                     sprintf(kb_msg,"M%2d",fd-4);
                     msg[result]='\0';
                     
@@ -223,41 +223,41 @@ int main(int argc, char *argv[]) {
 						flaga=1;
 					}
 					
-                    /*concatinate the client id with the client's message*/
+                    /*sklejenie id i wiadomosci w jeden string*/
 					strcat(kb_msg," ");
-                    strcat(kb_msg,msg+1);                                        
+                    strcat(kb_msg,msg);                                        
                     
-                    /*print to other clients*/
+                    /*wypisanie do innych klientow*/
 					if(flaga==0)
 					{
 						for(i=0;i<num_clients;i++){
-						   if (fd_array[i] != fd)  /*dont write msg to same client*/
+						   if (fd_array[i] != fd)  /*nie piszemy do tego kto wyslal wiadomosc*/
 							  write(fd_array[i],kb_msg,strlen(kb_msg));
 						}
-						/*print to server*/
+						/*wypisanie na serwerze*/
 						printf("%s",kb_msg+1);
 					}
 					else if (flaga==1)
 					{						
 						odbierzPlik(client_sockfd);
 						for(i=0;i<num_clients;i++){
-						   if (fd_array[i] != fd)  /*dont write msg to same client*/
+						   if (fd_array[i] != fd)  
 							  wyslijPlik(fd_array[i]);
 						}
 						flaga=0;
 					}						
-                     /*Exit Client*/
+                     /*wylaczenie klienta*/
                     if(msg[0] == 'X'){
                        exitClient(fd,&readfds, fd_array,&num_clients);
                     }   
                  }                                   
               }                  
-              else {  /* A client is leaving */
+              else { 
                  exitClient(fd,&readfds, fd_array,&num_clients);
               }//if
            }//if
         }//for
      }//while
-  }//end Server code
+  }//koniec kodu serwera
 
 }//main
